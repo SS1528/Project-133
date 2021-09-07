ece7c614decdbe9a01cdfdcc8953385792621242
SS1528 <79801545+SS1528@users.noreply.github.com>
2021-09-07 09:30:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bed_room(){
+    window.location="bed_room.html";
+}
+function tv(){
+    window.location="tv.html";
+}
+function desk(){
+    window.location="desk.html";
+}
+function fruit_basket(){
+    window.location="fruit_basket.html";
+}
+function phone(){
+    window.location="phone.html";
+}
